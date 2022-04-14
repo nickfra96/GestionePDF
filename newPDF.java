1adfa89dc1920c36c317df060824683dc9a9130f
nickfra96 <69160276+nickfra96@users.noreply.github.com>
2022-04-14 20:16:24 +0200

    Create newPDF.java

---
+++ newPDF.java
@@ -0,0 +1,31 @@
+package it.francesco.progetti.gestorePDF;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class JavaPdfHelloWorld
+{
+   public static void main(String[] args)
+   {
+      Document documento = new Document();
+      try
+      {
+         PdfWriter writer = PdfWriter.getInstance(documento, new FileOutputStream("test.pdf"));
+         documento.open();
+         documento.add(new Paragraph("Ciao questo è un pdf."));
+         documento.close();
+         writer.close();
+      } catch (DocumentException e)
+      {
+         e.printStackTrace();
+      } catch (FileNotFoundException e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
