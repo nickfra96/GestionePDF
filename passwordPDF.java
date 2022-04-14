4e68ed276c3be53ae39ca741917d33a150c53288
nickfra96 <69160276+nickfra96@users.noreply.github.com>
2022-04-14 20:23:15 +0200

    Update passwordPDF.java

--- passwordPDF.java
+++ passwordPDF.java
@@ -3,7 +3,6 @@ package it.francesco.progetti.gestorePDF;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.util.Date;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
