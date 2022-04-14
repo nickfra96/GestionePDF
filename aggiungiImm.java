28943b98b857d117c230985644e4c16b516b345b
nickfra96 <69160276+nickfra96@users.noreply.github.com>
2022-04-14 20:29:40 +0200

    Create aggiungiImm.java

---
+++ aggiungiImm.java
@@ -0,0 +1,32 @@
+package com.howtodoinjava.examples;
+
+import java.io.FileOutputStream;
+import java.net.URL;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class aggiungiImm {
+	public static void main(String[] args) {
+		Document documento = new Document(); 
+		try
+		{
+			PdfWriter writer = PdfWriter.getInstance(documento, new FileOutputStream("PDFimm.pdf"));
+			documento.open();
+			documento.add(new Paragraph("Immagine example"));
+
+			Image image1 = Image.getInstance("1.png");
+			image1.scaleAbsolute(200, 200);
+
+			documento.add(image1);
+
+			documento.close();
+			writer.close();
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
